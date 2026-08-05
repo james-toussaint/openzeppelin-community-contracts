86d98e9316f48b250519d85b10b24ebd6a1c6890
James Toussaint <33313130+james-toussaint@users.noreply.github.com>
2026-08-05 11:47:21 +0200

    Remove internal

--- contracts/crosschain/wormhole/WormholeGatewayAdapter.sol
+++ contracts/crosschain/wormhole/WormholeGatewayAdapter.sol
@@ -33,8 +33,8 @@ contract WormholeGatewayAdapter is IERC7786GatewaySource, IWormholeReceiver, Own
         bytes payload;
     }
 
-    IWormholeRelayer internal immutable _wormholeRelayer;
-    uint16 internal immutable _wormholeChainId;
+    IWormholeRelayer immutable _wormholeRelayer;
+    uint16 immutable _wormholeChainId;
     uint24 private constant EVM_ID_FLAG = 1 << 16;
 
     // Remote gateway.
